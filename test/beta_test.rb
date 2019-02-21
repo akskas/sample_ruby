590b69ff46b8a37351464e09f35d244931d8a9b7
ankul-shippable <ankul@shippable.com>
2019-02-21 15:18:46 +0530

    Create beta_test.rb

---
+++ test/beta_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+class AlphaTest < Test::Unit::TestCase
+  def test_passing
+    assert(true)
+  end
+  
+  def test_passing_a
+    assert(true)
+  end
+  
+  def test_passing_b
+    assert(true)
+  end
+  
+end
