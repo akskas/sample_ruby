b1945b1e77fd800838e37c75f079e82575494e19
ankul-shippable <ankul@shippable.com>
2019-02-21 15:16:07 +0530

    Update alpha_test.rb

--- test/alpha_test.rb
+++ test/alpha_test.rb
@@ -4,4 +4,13 @@ class AlphaTest < Test::Unit::TestCase
   def test_passing
     assert(true)
   end
+  
+  def test_passing_a
+    assert(true)
+  end
+  
+  def test_passing_b
+    assert(true)
+  end
+  
 end
