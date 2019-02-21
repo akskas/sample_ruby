340c06bebaeae7b5b029b1ecb3df74b40bb70cfd
ankul-shippable <ankul@shippable.com>
2019-02-21 14:21:42 +0530

    Update test_alpha.rb

--- test/test_alpha.rb
+++ test/test_alpha.rb
@@ -4,16 +4,4 @@ class AlphaTest < Test::Unit::TestCase
   def test_passing
     assert(true)
   end
-
-  def test_failing
-    assert(false, "Failure!")
-  end
-
-  def test_error
-    raise "Error!"
-  end
-
-  def test_pending
-    pend
-  end
 end
